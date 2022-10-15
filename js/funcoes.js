59e6c4dcc100a720a182641d9d9b3e2ca488c11a
Vinicius Felipe <vfpv@hotmail.com>
2022-10-15 18:02:16 -0300

    update

--- js/funcoes.js
+++ js/funcoes.js
@@ -12,7 +12,7 @@ Jcrop.load('alvo').then(img => {
     // arrayCampos.push({ widget: jcp.newWidget(Jcrop.Rect.create(580, 500, 360, 110)), nome: 'maria' });
 
     jcp.listen('crop.activate', (widget) => {
-        console.log(widget.pos)
+        //console.log(widget.pos)
     });
 
     jcp.addClass('jcrop-ux-keep-current');
@@ -43,6 +43,15 @@ function newUsuario() {
     // });
 
     console.log(usuario);
+    console.log(arrayCampos);
+}
+
+function removeUsuario() {
+    
+    arrayCampos.splice(jcp.removeWidget(jcp.active));
+
+    console.log(arrayCampos);
+
 }
 
 function GetCorRGB() {
@@ -58,56 +67,3 @@ function GetCorRGB() {
 
     return lCor;
 }
-
-//CROP 2
-
-function setImage(tag) {
-    document.getElementById('target').src = 'img/' + tag;
-}
-
-var stage;
-Jcrop.load('target').then(img => {
-    stage = Jcrop.attach(img, { multi: true });
-    stage.listen('crop.remove', function (widget) {
-        const pos = widget.pos;
-        console.log(pos.x, pos.y, pos.w, pos.h);
-    });
-});
-
-function eventoUpdate() {
-    stage.listen('crop.update', function (widget, e) {
-        const pos = widget.pos;
-        console.log(pos.x, pos.y, pos.w, pos.h);
-    });
-}
-
-function eventoActivate() {
-    stage.listen('crop.activate', function (widget, e) {
-        const pos = widget.pos;
-        console.log(pos.x, pos.y, pos.w, pos.h);
-    });
-}
-
-function eventoChange() {
-    stage.listen('crop.change', function (widget, e) {
-        const pos = widget.pos;
-        console.log(pos.x, pos.y, pos.w, pos.h);
-    });
-}
-
-function eventoRemove() {
-    stage.listen('crop.remove', function (widget, e) {
-        const pos = widget.pos;
-        console.log(pos.x, pos.y, pos.w, pos.h);
-    });
-}
-
-//CROP 3
-
-var jcp3;
-const myImage = new Image();
-myImage.src = 'https://s1.static.brasilescola.uol.com.br/be/2021/11/planeta-marte.jpg';
-document.getElementById('container').appendChild(myImage);
-Jcrop.load(myImage).then(img => {
-    jcp3 = Jcrop.attach(img, { shadeColor: 'pink' })
-});
