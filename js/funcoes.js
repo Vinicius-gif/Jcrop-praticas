6e7eaf5f94796c8d929394d48730d63a557e38f8
Vinicius Felipe <vfpv@hotmail.com>
2022-10-21 10:01:57 -0300

    up

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,16 +1,25 @@
 //CROP 1
 var jcp;
+var numUsuario = 0;
 var arrayCampos = [];
 
 Jcrop.load('alvo').then(img => {
     jcp = Jcrop.attach(img, { multi: false, canRemove: false });
 
+    const rect = Jcrop.Rect.sizeOf(jcp.el);
+
+    jcp.newWidget(rect.scale(.7, .5).center(rect.w, rect.h));
+    
     jcp.addClass('jcrop-ux-keep-current');
+
 });
 
 function ativarPosicao(pUsuario) {
 
     jcp.activate(arrayCampos[pUsuario].widget);
+
+    //console.log(jcp.activate);
+    //console.log(arrayCampos);
 
 }
 
@@ -18,13 +27,24 @@ function newUsuario() {
 
     var usuario = prompt("Digite o nome do usuário");
 
-    arrayCampos.push({ widget: jcp.newWidget(Jcrop.Rect.create(380, 100, 360, 110)), nome: usuario });
+    let x = jcp.active.pos.x, y = jcp.active.pos.y, w = jcp.active.pos.w, h =  jcp.active.pos.h;
 
-    var index = arrayCampos.length - 1;
+    //console.log(x,y,w,h);
+
+    arrayCampos.push({ widget: jcp.newWidget(Jcrop.Rect.create( x, y, w, h)), nome: usuario });
+    //Jcrop.Rect.create(380, 100, 360, 110)
+
+    //var index = arrayCampos.length - 1;
 
     var lista = document.getElementById("lista-pessoas").innerHTML;
-    var lista = lista + "<li class = 'list-btns-users' id = (" + index + ") ><button class = 'lista-usuario' style = 'background-color: rgb(" + GetCorRGB() + ")' onclick= 'ativarPosicao(" + index + ")'>" + usuario + "</button> <button id = 'btn-excluir' onclick= 'removeUsuario(" + index + ")'> X </button></li>";
+
+    var lista = lista + "<li class = 'list-btns-users' id = 'L-" + numUsuario + "' ><button class = 'lista-usuario' style = 'background-color: rgb(" + GetCorRGB() + ")' onclick= 'ativarPosicao(" + index + ")'>" + usuario + "</button> <button id = 'btn-excluir' onclick= 'removeUsuario(" + index + ")'> X </button><button id = 'btn-editar' onclick= 'alterarUsuario(" + index + ")'> Editar </button></li>";
+    
+    numUsuario++;
+
     document.getElementById("lista-pessoas").innerHTML = lista;
+
+    //console.log(jcp.active);
 
 }
 
@@ -32,7 +52,7 @@ function removeUsuario(pIndex) {
 
     jcp.setOptions({ canRemove: true });
 
-    var item = document.querySelector(".list-btns-users").item(pIndex);
+    var item = document.getElementsByClassName("list-btns-users")[pIndex];
 
     console.log(item);
 
@@ -59,4 +79,18 @@ function GetCorRGB() {
     var lCor = lRed.toString() + "," + lGreen.toString() + "," + lBlue.toString();
 
     return lCor;
+}
+
+function alterarUsuario(pIndex) {
+
+    var editUser = prompt("Digite o nome do usuário");
+
+    arrayCampos[pIndex].nome = editUser;
+
+    console.log(arrayCampos);
+
+    user = document.querySelector(".lista-usuario")[pIndex];
+
+    //user.innerHTML = "<button class = 'lista-usuario' style = 'background-color: rgb(" + GetCorRGB() + ")' onclick= 'ativarPosicao(" + pIndex + ")'>" + editUser + "</button>"
+
 }
